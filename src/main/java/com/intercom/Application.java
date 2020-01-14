5f7fdcd581fecab142792b12ef221fc52f533ae1
Irin Akhtar <4187807+irinakhtar@users.noreply.github.com>
2020-01-14 22:58:07 +0000

    add model and two methods for calculating distanced

--- src/main/java/com/intercom/Application.java
+++ src/main/java/com/intercom/Application.java
@@ -1,6 +1,25 @@
 package com.intercom;
 
+import com.intercom.model.Customer;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Application {
+
+    public static ArrayList<Customer> getCustomersFromFile() {
+        // read the file
+        //map to Customer object
+        return null;
+    }
+
+    public static ArrayList<Customer> getClosestCustomer(ArrayList<Customer> customers) {
+        double latitude = 53.339428;
+        double longitude = -6.257664;
+        // calculate distance and find nearest customer list
+        return null;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
---
+++ src/main/java/com/intercom/model/Customer.java
@@ -0,0 +1,47 @@
+package com.intercom.model;
+
+public class Customer {
+    private int userId;
+    private String name;
+    private float latitude;
+    private float longitude;
+
+    public Customer(int userId, String name, float latitude, float longitude) {
+        this.userId = userId;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+}
